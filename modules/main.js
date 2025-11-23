0f2117dd58508c58efef9cea7bd8934faddd0e01
tmtabor <tmtabor@cloud.ucsd.edu>
2025-11-22 22:11:35 -0800

    Set saga machine config dynamically

--- modules/main.js
+++ modules/main.js
@@ -1,5 +1,6 @@
-import { init_saga_machine } from "../saga-machine-foundryvtt/modules/main.js";
+import { init_saga_machine } from "../saga-machine-foundryvtt/modules/init.js";
 
+// Initialize Saga Machine with Shadows Over Sol config
 init_saga_machine({
     level: "150",
     luck_label: "Edge",
